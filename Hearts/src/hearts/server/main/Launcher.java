21ccca40ab43ea92bd8f42e041d7b33cb0f8fe8a
Nathan <natyconnor@gmail.com>
2013-10-15 00:39:29 -0700

    Refactor Server to be more conducive to multi-threading

--- Hearts/src/hearts/server/main/Launcher.java
+++ Hearts/src/hearts/server/main/Launcher.java
@@ -1,81 +1,11 @@
 package hearts.server.main;
 
-import hearts.client.player.ComputerPlayer;
-import hearts.client.player.HumanPlayer;
-import hearts.server.game.*;
-
 public class Launcher {
 
 	public static void main(String[] args) {
 		
 		//Set up server (which sets up game)
-		Server server = new Server();
-		
-		//Add players to server/game
-		//HumanPlayer h1 = new HumanPlayer("Player1");
-		ComputerPlayer h1 = new ComputerPlayer("Player1");
-		ComputerPlayer h2 = new ComputerPlayer("Player2");
-		ComputerPlayer h3 = new ComputerPlayer("Player3");
-		ComputerPlayer h4 = new ComputerPlayer("Player4");
-		
-		h1.setServer(server);
-		h2.setServer(server);
-		h3.setServer(server);
-		h4.setServer(server);
-		
-		server.getGame().addPlayer(h1);
-		server.getGame().addPlayer(h2);
-		server.getGame().addPlayer(h3);
-		server.getGame().addPlayer(h4);
-		
-		
-		//Game loop; while high score is less than 100
-		while(server.getGame().getHighScore() < 100)
-		{
-			//New Game
-			//shuffle cards
-			server.getGame().getDeck().shuffle();
-			//deal them
-			server.getGame().dealHand();
-			
-			//Have players pass cards
-			if(server.getGame().getPassingDirection() != PassDirection.KEEP)
-			{
-				server.getGame().promptPassCards();
-				server.getGame().givePassCards();
-			}
-			
-			//while there are still rounds
-			while(server.getGame().getRoundsPlayed() < Game.numRounds)
-			{
-			
-				//ask each player to play a card
-				server.getGame().promptPlayCard();
-				
-				//see who takes trick
-				int winningPlayer = server.getGame().getTable().takeTrick();
-				
-				//add points to their game score
-				server.getGame().addTrickPoints(winningPlayer);
-				
-				//clear table
-				server.getGame().getTable().clearTable();
-				
-				server.getGame().incrementRound();
-				
-				System.out.println("\n-----------------------------------------\n");
-
-			}
-			
-			//update match score from game score
-			server.getGame().updateMatchScore();
-			
-			//reset for next game
-			server.getGame().newGame();
-		}
-		
-		System.out.println("Game Over!");
-		System.out.println(server.getGame().getWinningPlayer().toString() + " wins!!");
+		new Server();
 	}
 
 }
--- Hearts/src/hearts/server/main/Server.java
+++ Hearts/src/hearts/server/main/Server.java
@@ -1,6 +1,8 @@
 package hearts.server.main;
 
+import hearts.client.player.ComputerPlayer;
 import hearts.server.game.Game;
+import hearts.server.game.PassDirection;
 
 public class Server {
 	private Game myGame;
@@ -8,6 +10,78 @@ public class Server {
 	public Server()
 	{
 		myGame = new Game();
+		
+		//temp setup for one game and one thread
+		//Add players to server/game
+				//HumanPlayer h1 = new HumanPlayer("Player1");
+		ComputerPlayer h1 = new ComputerPlayer("Player1");
+		ComputerPlayer h2 = new ComputerPlayer("Player2");
+		ComputerPlayer h3 = new ComputerPlayer("Player3");
+		ComputerPlayer h4 = new ComputerPlayer("Player4");
+		
+		h1.setServer(this);
+		h2.setServer(this);
+		h3.setServer(this);
+		h4.setServer(this);
+		
+		myGame.addPlayer(h1);
+		myGame.addPlayer(h2);
+		myGame.addPlayer(h3);
+		myGame.addPlayer(h4);
+		
+		runGame(myGame);
+	}
+	
+	private void runGame(Game g)
+	{
+		//Game loop; while high score is less than 100
+		while(g.getHighScore() < 100)
+		{
+			//New Game
+			//shuffle cards
+			g.getDeck().shuffle();
+			//deal them
+			g.dealHand();
+			
+			//Have players pass cards
+			if(g.getPassingDirection() != PassDirection.KEEP)
+			{
+				g.promptPassCards();
+				g.givePassCards();
+			}
+			
+			//while there are still rounds
+			while(g.getRoundsPlayed() < Game.numRounds)
+			{
+			
+				//ask each player to play a card
+				g.promptPlayCard();
+				
+				//see who takes trick
+				int winningPlayer = g.getTable().takeTrick();
+				
+				//add points to their game score
+				g.addTrickPoints(winningPlayer);
+				
+				//clear table
+				g.getTable().clearTable();
+				
+				g.incrementRound();
+				
+				System.out.println("\n-----------------------------------------\n");
+
+			}
+			
+			//update match score from game score
+			g.updateMatchScore();
+			
+			//reset for next game
+			g.newGame();
+		}
+		
+		System.out.println("Game Over!");
+		System.out.println(g.getWinningPlayer().toString() + " wins!!");
+
 	}
 	
 	public Game getGame()
